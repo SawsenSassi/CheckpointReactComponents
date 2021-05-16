d9f3fb52bb39f3fd21bec229ee5ccb4058a07624
sawsen <sawsensassi97@gmail.com>
2021-05-16 04:04:57 -0700

    Use Class components and functional components

---
+++ src/Component/Profile/Address.js
@@ -0,0 +1,12 @@
+import './Profile.css'
+
+const Address = () => {
+    return (
+        <>
+        <h2>My address is :</h2>
+        <p>17 rue thimar Rades 2040.</p>
+        </>
+    )
+}
+
+export default Address
---
+++ src/Component/Profile/FullName.js
@@ -0,0 +1,13 @@
+import './Profile.css'
+
+const FullName = () => {
+    return (
+        <>
+        <h1>My name is :</h1>
+         <p> Sawsen Sassi.</p>
+
+        </>
+    )
+}
+
+export default FullName
